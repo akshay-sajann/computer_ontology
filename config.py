2820d6915dd587431c5b71fd6af472464a820a4f
Akshay Sajan <akshaysajan333@gmail.com>
2024-05-13 17:02:39 +0200

    made a raw folder and also started a config file to store path and dir variables

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py 
+
+from pathlib import Path
+
+computer_dir = Path('./data/computer')
+expert_dir = Path('./data/expert')
+
+raw_path = computer_dir / 'alldesc_dataset.csv'
+
+computer_dataset_path = computer_dir / 'computer_dataset.csv'
+
+expert_dataset_path = expert_dir / 'expert_dataset.csv'
+
+canon_path = computer_dir / 'canonicalization_and_umbrella-terms/labels-canonicalization.xlsx'
+
+computer_path = computer_dir / 'canonicalization_and_umbrella-terms/computer_derived_ontology_11.xlsx'
+
+lemma_path = expert_dir / 'lemmatization_and_umbrella-terms/computer_derived_ontology_11.xlsx'
+
+expert_path = expert_dir / 'canonicalization_and_umbrella-terms/computer_derived_ontology_11.xlsx'
