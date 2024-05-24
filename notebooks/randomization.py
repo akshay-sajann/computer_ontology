e128d7d968dc1c3e41bfcfe073849ba512faf58f
Akshay Sajan <akshaysajan333@gmail.com>
2024-05-24 10:55:02 +0200

    optimize randomization.py

--- notebooks/randomization.py
+++ notebooks/randomization.py
@@ -4,7 +4,8 @@ import pandas as pd
 from computer_ontology.custom_funcs import *
 from sklearn.ensemble import RandomForestClassifier
 from computer_ontology.featurizer import get_mordred
-from computer_ontology.config import raw_path, computer_dataset_path
+from sklearn.preprocessing import MultiLabelBinarizer
+from computer_ontology.config import raw_path, computer_dataset_path, computer_path
 from torchmetrics.classification import MultilabelF1Score, MultilabelAUROC, MultilabelPrecision, MultilabelRecall
 
 # dictionary to store umbrella scores
@@ -72,3 +73,82 @@ umbrella_scores['auroc_weighted'].append(auroc_weighted)
 umbrella_scores['precision_weighted'].append(precision_weighted)
 umbrella_scores['recall_weighted'].append(recall_weighted)
 
+rand_scores = {}
+rand_scores['f1_macro'] = []
+rand_scores['auroc_macro'] = []
+rand_scores['precision_macro'] = []
+rand_scores['recall_macro'] = []
+rand_scores['f1_micro'] = []
+rand_scores['auroc_micro'] = []
+rand_scores['precision_micro'] = []
+rand_scores['recall_micro'] = []
+rand_scores['f1_weighted'] = []
+rand_scores['auroc_weighted'] = []
+rand_scores['precision_weighted'] = []
+rand_scores['recall_weighted'] = []
+
+labels_df = pd.read_excel(computer_path)
+labels_to_remove = labels_df[labels_df['Umbrella Terms'].isna()]['Original Descriptors']
+labels_df = labels_df.dropna()
+
+trials = 2
+for count in range(trials):
+    replace = labels_df.copy()
+    replace['Umbrella Terms'] = np.random.permutation(replace['Umbrella Terms'])
+    replace = replace.values.tolist()
+    # Changing to Umbrella terms and normalizing it once again
+    rand = dataset.copy()
+    rand['Descriptors'] = rand['Descriptors'].apply(lambda x: ';'.join([item for item in x.split(';') if item not in labels_to_remove.index]))
+    rand = rand[rand['Descriptors'] != '']
+    rand['Descriptors'] = rand['Descriptors'].apply(check_and_replace)
+    rand['Descriptors'] = rand['Descriptors'].apply(make_unique)
+    rand['Descriptors'] = rand['Descriptors'].dropna()
+    rand = rand[rand['Descriptors'] != '']
+    #rand['Descriptor Count'] = rand['Descriptors'].apply(lambda x: len(x.split(';')))
+    rand['Descriptors'] = rand['Descriptors'].apply(lambda x: x.split(';'))
+    mlb = MultiLabelBinarizer(sparse_output=True)
+    mlb.fit(rand['Descriptors'])
+    rand = rand.join(pd.DataFrame.sparse.from_spmatrix(mlb.transform(rand['Descriptors']), index=rand.index, columns=mlb.classes_))
+    X, y = branch_split(mordred, rand)
+    train_x, train_y, test_x, test_y = iterative_train_test_split(X, y, 0.2)
+    clf = RandomForestClassifier(random_state=0)
+    clf.fit(train_x, np.asarray(train_y, dtype=np.float64))
+
+    y_hat = clf.predict(test_x)
+
+    f1score_macro = MultilabelF1Score(num_labels=len(train_y.columns), average="macro")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.float))
+    auroc_macro = MultilabelAUROC(num_labels=len(train_y.columns), average="macro")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+    precision_macro = MultilabelPrecision(num_labels=len(train_y.columns), average="macro")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+    recall_macro = MultilabelRecall(num_labels=len(train_y.columns), average="macro")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+
+    f1score_micro = MultilabelF1Score(num_labels=len(train_y.columns), average="micro")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.float))
+    auroc_micro = MultilabelAUROC(num_labels=len(train_y.columns), average="micro")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+    precision_micro = MultilabelPrecision(num_labels=len(train_y.columns), average="micro")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+    recall_micro = MultilabelRecall(num_labels=len(train_y.columns), average="micro")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+
+    f1score_weighted = MultilabelF1Score(num_labels=len(train_y.columns), average="weighted")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.float))
+    auroc_weighted = MultilabelAUROC(num_labels=len(train_y.columns), average="weighted")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+    precision_weighted = MultilabelPrecision(num_labels=len(train_y.columns), average="weighted")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+    recall_weighted = MultilabelRecall(num_labels=len(train_y.columns), average="weighted")(torch.tensor(y_hat, dtype=torch.float), torch.tensor(test_y.values, dtype=torch.long))
+
+    rand_scores['f1_macro'].append(float(f1score_macro))
+    rand_scores['auroc_macro'].append(float(auroc_macro))
+    rand_scores['precision_macro'].append(float(precision_macro))
+    rand_scores['recall_macro'].append(float(recall_macro))
+    rand_scores['f1_micro'].append(float(f1score_micro))
+    rand_scores['auroc_micro'].append(float(auroc_micro))
+    rand_scores['precision_micro'].append(float(precision_micro))
+    rand_scores['recall_micro'].append(float(recall_micro))
+    rand_scores['f1_weighted'].append(float(f1score_weighted))
+    rand_scores['auroc_weighted'].append(float(auroc_weighted))
+    rand_scores['precision_weighted'].append(float(precision_weighted))
+    rand_scores['recall_weighted'].append(float(recall_weighted))
+
+for key in umbrella_scores:
+   z_statistic, p_value = stests.ztest(rand_scores[key], value=umbrella_scores[key])
+   print(key)
+   print("Umbrella score", umbrella_scores[key])
+   print("Random scores mean", statistics.mean(rand_scores[key]))
+   print("Random scores stdev", statistics.stdev(rand_scores[key]))
+   print("Z-statistic:", z_statistic)
+   print("P-value:", p_value)
