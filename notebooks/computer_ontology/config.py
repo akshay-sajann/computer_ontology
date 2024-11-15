22ee552448bc81922560fd9f680a308fb5286dd3
Akshay <akshaysajan333@gmail.com>
2024-11-15 16:26:48 +0100

    reorder config.py (I)

--- notebooks/computer_ontology/config.py
+++ notebooks/computer_ontology/config.py
@@ -2,26 +2,48 @@
 
 from pathlib import Path
 
-computer_dir = Path('../data/computer')
-expert_dir = Path('../data/expert')
-result_path = Path('../results')
+data = Path('../data')
+result = Path('../results')
 
-lemma_raw_path = expert_dir / 'dataset/raw/lemma_alldesc_dataset.csv'
+# train 
 
-canon_raw_path = computer_dir / 'dataset/raw/canon_alldesc_dataset.csv'
+alldesc_train_path = data / "train/alldesc_train.csv"
+computer_train_path = data / "train/computer_train.csv"
+expert_train_path = data / "train/expert_train.csv"
+lemma_train_path = data / "train/lemma_train.csv"
 
-computer_dataset_path = computer_dir / 'dataset/computer_dataset_11.csv'
+# test 
 
-expert_dataset_path = expert_dir / 'dataset/expert_dataset.csv'
+alldesc_test_path = data / "test/alldesc_test.csv"
+computer_test_path = data / "test/computer_test.csv"
+expert_test_path = data / "test/expert_test.csv"
+lemma_test_path = data / "test/lemma_test.csv"
 
-canon_path = computer_dir / 'canonicalization_and_umbrella-terms/labels_canonicalization.xlsx'
+# ontology 
 
-computer_path = computer_dir / 'canonicalization_and_umbrella-terms/computer_derived_ontology_11.xlsx'
+computer_path = data / "ontology/computer/computer_ontology_Aug.xlsx" 
+canon_path = data / "ontology/computer/labels_canonicalization.xlsx" 
 
-lemma_path = expert_dir / 'lemmatization_and_umbrella-terms/labels-to-be-lemmatized.txt'
+expert_path = data / "ontology/expert/expert_derived_ontology.csv"
+lemma_path =  data / "ontology/expert/labels-to-be-lemmatized.txt"
 
-expert_path = expert_dir / 'lemmatization_and_umbrella-terms/expert_derived_ontology.csv'
+# randomization
 
-comp_result_path = result_path / 'comp_scores.pkl'
+comp_rand_xg_path = result / "comp_rand_xg.pkl"
+comp_rand_rf_path = result / "comp_rand_rf.pkl"
+comp_rand_logreg_path = result / "comp_rand_logreg.pkl"
 
-exp_result_path = result_path / 'exp_scores.pkl'
+exp_rand_xg_path = result / "exp_rand_xg.pkl"
+exp_rand_rf_path = result / "exp_rand_rf.pkl"
+exp_rand_logreg_path = result / "exp_rand_logreg.pkl"
+
+# SHAP
+
+
+
+# Features
+
+
+# Hyperparameters
+
+
